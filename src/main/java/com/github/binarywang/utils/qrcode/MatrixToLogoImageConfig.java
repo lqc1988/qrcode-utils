f5c588daa2d7839077886b2badb22e69f384a0ac
lqc1988 <lqclh502@sina.com>
2020-05-25 13:43:25 +0800

    精简代码1

--- src/main/java/com/github/binarywang/utils/qrcode/MatrixToLogoImageConfig.java
+++ src/main/java/com/github/binarywang/utils/qrcode/MatrixToLogoImageConfig.java
@@ -9,35 +9,48 @@ import java.awt.*;
  * </pre>
  */
 public class MatrixToLogoImageConfig {
-  //logo默认边框颜色
-  public static final Color DEFAULT_BORDERCOLOR = Color.RED;
-  //logo默认边框宽度
-  public static final int DEFAULT_BORDER = 2;
-  //logo大小默认为照片的1/5
-  public static final int DEFAULT_LOGOPART = 5;
+    //logo默认边框颜色
+    public static final Color DEFAULT_BORDER_COLOR = Color.RED;
+    //logo默认边框宽度
+    public static final int DEFAULT_BORDER = 2;
+    //logo大小默认为照片的1/5
+    public static final int DEFAULT_LOGO_PART = 5;
 
-  private final int border = DEFAULT_BORDER;
-  private final Color borderColor;
-  private final int logoPart;
+    private final int border;
+    private final Color borderColor;
+    private final int logoPart;
 
-  public MatrixToLogoImageConfig() {
-    this(DEFAULT_BORDERCOLOR, DEFAULT_LOGOPART);
-  }
+    public MatrixToLogoImageConfig() {
+        this(DEFAULT_BORDER, DEFAULT_BORDER_COLOR, DEFAULT_LOGO_PART);
+    }
 
-  public MatrixToLogoImageConfig(Color borderColor, int logoPart) {
-    this.borderColor = borderColor;
-    this.logoPart = logoPart;
-  }
+    public MatrixToLogoImageConfig(int border) {
+        this.border = border;
+        this.borderColor = DEFAULT_BORDER_COLOR;
+        this.logoPart = DEFAULT_LOGO_PART;
+    }
 
-  public Color getBorderColor() {
-    return this.borderColor;
-  }
+    public MatrixToLogoImageConfig(int border, Color borderColor) {
+        this.border = border;
+        this.borderColor = borderColor;
+        this.logoPart = DEFAULT_LOGO_PART;
+    }
 
-  public int getBorder() {
-    return this.border;
-  }
+    public MatrixToLogoImageConfig(Integer border, Color borderColor, Integer logoPart) {
+        this.border = border;
+        this.borderColor = borderColor;
+        this.logoPart = logoPart;
+    }
 
-  public int getLogoPart() {
-    return this.logoPart;
-  }
+    public Color getBorderColor() {
+        return this.borderColor;
+    }
+
+    public int getBorder() {
+        return this.border;
+    }
+
+    public int getLogoPart() {
+        return this.logoPart;
+    }
 }
